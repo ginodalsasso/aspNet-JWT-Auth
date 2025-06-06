c78c83233307b5666060a395e521de7e0d3d9c4c
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-06-06 14:13:10 +0200

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using aspNet_JWT_Auth.Entities;
+using aspNet_JWT_Auth.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspNet_JWT_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.PasswordHash);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+
+            var passwordVerificationResult = new PasswordHasher<User>()
+                .VerifyHashedPassword(user, user.PasswordHash, request.PasswordHash);
+
+            if (passwordVerificationResult == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong credentials");
+            }
+
+            string token = "success";
+
+            return Ok(token);
+        }
+    }
+}
---
+++ Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace aspNet_JWT_Auth.Models
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
