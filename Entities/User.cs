447f666e8366f4d1b2c0543656a3a83bab755c78
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-06-06 16:27:55 +0200

    NEW: Role in User Entity.

--- Entities/User.cs
+++ Entities/User.cs
@@ -6,5 +6,7 @@
 
         public string Username { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "User"; // Default role
     }
 }
